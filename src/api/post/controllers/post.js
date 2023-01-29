1eb9666c3e90433de95a28297f4d4468033a00ed
Jordi Irawan <jordirwn@gmail.com>
2023-01-29 11:38:05 +0700

    update

--- src/api/post/controllers/post.js
+++ src/api/post/controllers/post.js
@@ -9,6 +9,22 @@ const isMe = require("../../../utils/isMe");
 const isOwner = require("../../../utils/isOwner");
 
 module.exports = createCoreController("api::post.post", ({ strapi }) => ({
+  async find(ctx) {
+    const relations = await strapi.entityService.findMany(
+      "api::relation.relation",
+      {
+        filters: {
+          owner: { id: ctx.state.user.id },
+        },
+        populate: "*",
+      }
+    );
+    const postOwnerIds = relations.map((relation) => relation.to.id);
+    postOwnerIds.push(ctx.state.user.id);
+    ctx.query.filters = { owner: postOwnerIds };
+    const res = await super.find(ctx);
+    return res;
+  },
   async create(ctx) {
     ctx.request.body.data.owner = ctx.state.user.id;
     const res = await super.create(ctx);
--- src/api/relation/controllers/relation.js
+++ src/api/relation/controllers/relation.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const isOwner = require("../../../utils/isOwner");
-
+const utils = require("@strapi/utils");
 /**
  * relation controller
  */
@@ -19,6 +19,25 @@ module.exports = createCoreController(
     async update(ctx) {
       await isOwner(ctx, "api::relation.relation");
       const res = await super.update(ctx);
+      return res;
+    },
+    async create(ctx) {
+      const relations = await strapi.entityService.findMany(
+        "api::relation.relation",
+        {
+          populate: "*",
+          filters: {
+            owner: { id: ctx.state.user.id },
+            to: { id: ctx.request.body.data.to },
+          },
+        }
+      );
+      if (relations.length) {
+        throw new utils.errors.ValidationError();
+      }
+
+      ctx.request.body.data.owner = ctx.state.user.id;
+      const res = await super.create(ctx);
       return res;
     },
   })
